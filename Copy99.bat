b54ff59f31188f5af974e820245c3ec59d62332e
goshinh <jiller@126.com>
2018-04-08 10:05:27 +0800

    国际化

--- Copy99.bat
+++ Copy99.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
-rem 作者:IT达摩
-rem 功能:CopyDir.bat批量复制文件夹
-rem 创建时间：2018-04-02 13:20
-rem 最后修改时间:2018-04-02 14:39
-rem 输入文件:班级名单.txt
+rem Author:ITDAMAO
+rem Feature:Copy99.bat copys dir
+rem Created Time：2018-04-02 13:20
+rem Last Modified Time:2018-04-08 10:39
+rem inputfile:01ListofClasses.txt
 
 setlocal EnableDelayedExpansion
-for /f "tokens=* delims=" %%i in (班级名单.txt) do (
+for /f "tokens=* delims=" %%i in (01ListofClasses.txt) do (
 set className=%%i
-xcopy 各班级作业收集-复制用 .\!className!\  /e /s
+xcopy 99CollectionofHomeworkforCopy .\!className!\  /e /s
 )
 pause
