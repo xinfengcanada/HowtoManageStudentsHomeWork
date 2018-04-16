e8c56c6591c550aa3875fdda76d1192ff7eccc5a
goshinh <jiller@126.com>
2018-04-16 11:28:18 +0800

    2018-04-16

--- 创建工作目录.bat
+++ 创建工作目录.bat
@@ -6,33 +6,33 @@ rem Last Modified Time:2018-04-10 10:51
 
 
 setlocal EnableDelayedExpansion
-rem 创建文件01班级清单.txt
-echo 创建文件01班级清单.txt
-if exist .\01班级清单.txt (echo 当前目录下已有01班级清单.txt存在) else (
-echo 当前目录下不存在01班级清单.txt,即将创建
-echo # 以#开头行是注释行,脚本不会读取>>01班级清单.txt
-echo.>>01班级清单.txt
-echo # "99-示例班级"作为缺省项,由程序自动创建>>01班级清单.txt
-echo 99-示例班级>>01班级清单.txt
-echo.>>01班级清单.txt)
+rem 创建文件A1班级清单.txt
+echo 创建文件A1班级清单.txt
+if exist .\A1班级清单.txt (echo 当前目录下已有A1班级清单.txt存在) else (
+echo 当前目录下不存在A1班级清单.txt,即将创建
+echo # 以#开头行是注释行,脚本不会读取>>A1班级清单.txt
+echo.>>A1班级清单.txt
+echo # "99-示例班级"作为缺省项,由程序自动创建>>A1班级清单.txt
+echo 99-示例班级>>A1班级清单.txt
+echo.>>A1班级清单.txt)
 
-rem 创建文件02课时清单.txt
-echo 创建文件02课时清单.txt
-if exist .\02课时清单.txt (echo 当前目录下已有02课时清单.txt存在) else (
-echo 当前目录下不存在02课时清单.txt,即将创建
-echo # 以#开头行是注释行,脚本不会读取>>02课时清单.txt
-echo.>>02课时清单.txt
-echo # "示例课时"作为缺省项,由程序自动创建>>02课时清单.txt
-echo 示例课时>>02课时清单.txt
-echo.>>02课时清单.txt)
+rem 创建文件A2课时清单.txt
+echo 创建文件A2课时清单.txt
+if exist .\A2课时清单.txt (echo 当前目录下已有A2课时清单.txt存在) else (
+echo 当前目录下不存在A2课时清单.txt,即将创建
+echo # 以#开头行是注释行,脚本不会读取>>A2课时清单.txt
+echo.>>A2课时清单.txt
+echo # "示例课时"作为缺省项,由程序自动创建>>A2课时清单.txt
+echo 示例课时>>A2课时清单.txt
+echo.>>A2课时清单.txt)
 
 rem 创建文件：各班级学生清单
 echo 创建文件：各班级学生清单
-set listOfStu=03各班级学生清单
+set listOfStu=A3各班级学生清单
 if exist .\!listOfStu! (echo 该目录已存在) else (
 mkdir .\!listOfStu!
 )
-for /f "eol=#" %%h in (01班级清单.txt) do (
+for /f "eol=#" %%h in (A1班级清单.txt) do (
 set cName=%%h
 echo !cName!
 if exist .\!listOfStu!\!cName!.txt (echo !listOfStu! 目录下已有 !cName!.txt存在) else (
@@ -44,18 +44,18 @@ echo.>>.\!listOfStu!\!cName!.txt)
 )
 
 rem 创建文件夹：各班级作业统计
-echo 创建文件夹：04各班级作业统计
-set countOfStu=04各班级作业统计
+echo 创建文件夹：A4各班级作业统计
+set countOfStu=A4各班级作业统计
 if exist .\!countOfStu! (echo 该目录已存在) else (
 mkdir .\!countOfStu!
 )
 
 rem 为每个班级创建用于收集学生作业的目录
 echo 为每个班级创建用于收集学生作业的目录
-for /f "eol=#" %%i in (01班级清单.txt) do (
+for /f "eol=#" %%i in (A1班级清单.txt) do (
 set className=%%i
 echo !className!
-for /f  "eol=#" %%j in (02课时清单.txt) do (
+for /f  "eol=#" %%j in (A2课时清单.txt) do (
 set lessonName=%%j
 mkdir .\!className!\!lessonName!\)
 
--- 统计学生作业.bat
+++ 统计学生作业.bat
@@ -3,43 +3,62 @@ rem 作者:IT达摩
 rem 功能:统计学生交作业的情况
 rem 命令说明：
 rem 统计学生作业 %1 %2,%1,%2两个必选参数.
-rem %1代表课时,如"Lesson1",%2代表班级,如"C5G6"即六（5）班
+rem %1代表课时,如"Lesson1",%2代表班级,如"1-65"即六（5）班
 rem 创建时间：2018-03-30 08:20
-rem 最后修改时间:2018-04-10 16:55
-rem 输入文件:03各班级学生清单\C5G6.txt
-rem 临时文件:result.txt,title_row.txt,sort_result.txt
-rem 输出文件:04各班级作业统计\Lesson1-C5G6.txt
+rem 最后修改时间:2018-04-16 11:20
+rem 输入文件:A3各班级学生清单\1-65.txt
+rem 临时文件:result.txt,title_row.txt,sort_result.txt,treeList.txt
+rem 输出文件:A4各班级作业统计\Lesson1_1-65_统计.txt
 
 setlocal EnableDelayedExpansion
 rem 已交学生序号
-set yesnum=0
+set yesnum=""
 rem 未交学生序号
-set nonum=0
-rem 设置文件名
-set filename=%1-%2-统计
+set nonum=""
+rem 学生总数
+set totalnum=0
 
-for /f "eol=#" %%i in (03各班级学生清单\%2.txt) do (
+rem 设置文件名
+set filename=%1_%2_统计
+tree /f .\%2\%1 >A4各班级作业统计\treeList.txt
+for /f "eol=#" %%i in (A3各班级学生清单\%2.txt) do (
 set str=%%i
 
 rem 在该目录和所有子目录中搜索包含!str!的每个文件，而不考虑字母的大小写
-findstr /s /i "\<!a!\>" .\%2\%1
-
+findstr  !str! A4各班级作业统计\treeList.txt
 rem 返回值为1=未找到时，格式化输出。
-IF  ERRORLEVEL 1 IF NOT ERRORLEVEL 2 set /a nonum+=1 & @ECHO 未交	!nonum!	%2	!str!>>result.txt
+IF  !ERRORLEVEL! NEQ 0 (
+set /a nocount+=1
+set /a nonum=!nocount!
+if !nonum! lss 10 (set nonum=0!nonum!)
+ECHO !nonum!	未交	%2	!str!>>noresult.txt)
+
 rem 返回值为0=找到时，格式化输出。
-IF  ERRORLEVEL 0 IF NOT ERRORLEVEL 1 set /a yesnum+=1 & @ECHO 已交	!yesnum!	%2	!str!>>result.txt
+IF  !ERRORLEVEL! EQU 0 (
+set /a yescount+=1
+set /a yesnum=!yescount! 
+if !yesnum! lss 10 (set yesnum=0!yesnum!)
+ECHO !yesnum!	已交	%2	!str!>>yesresult.txt)
 )
 rem 对生成的result.txt文件进行排序
-sort result.txt > sort_result.txt
-rem 删除临时文件:result.txt
-del result.txt
+sort  noresult.txt > sort_result.txt
+echo.>>sort_result.txt
+sort  yesresult.txt >> sort_result.txt
+
 rem 文件头部作成
 @echo 班    级	%2> title_row.txt
+set /a totalnum=!nocount!+!yescount!
+@echo 共    计	!totalnum!人>> title_row.txt
+@echo 课    时	%1>> title_row.txt
 @echo 作成时间	%Date:~0,4%-%Date:~5,2%-%Date:~8,2%>> title_row.txt
 @echo.>> title_row.txt
-@echo 状态	序号	班级	姓名>> title_row.txt
+@echo 序号	状态	班级	姓名>> title_row.txt
 
 rem 合并文件
-copy title_row.txt+sort_result.txt 04各班级作业统计\%filename%.txt
+copy title_row.txt+sort_result.txt A4各班级作业统计\%filename%.txt
 rem 删除临时文件:sort_result.txt,title_row.txt
-del sort_result.txt title_row.txt
+del noresult.txt
+del yesresult.txt
+del sort_result.txt 
+del title_row.txt
+del A4各班级作业统计\treeList.txt
