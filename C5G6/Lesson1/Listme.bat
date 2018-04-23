1eec39ba5de1cbe1e876a41ee184c7355462421c
goshinh <jiller@126.com>
2018-04-23 14:59:28 +0800

    修改Readme.md图片无法显示

--- C5G6/Lesson1/Listme.bat
+++ C5G6/Lesson1/Listme.bat
@@ -3,5 +3,6 @@ rem 作者:IT达摩
 rem 功能:统计本课时学生交作业的情况
 rem 创建时间：2018-04-23 13:43
 rem 最后修改时间:2018-04-23
+setlocal EnableDelayedExpansion 
 cd /d Z:\shareFolder\HowtoManageStudentsHomeWork\ 
 ListStatus Lesson1 C5G6 
